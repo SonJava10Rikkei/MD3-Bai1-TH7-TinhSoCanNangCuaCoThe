788c28c1125ee99f2cb137d4777b1661d0898c3a
SonJava10Rikkei <sonjapan7@gmail.com>
2023-03-05 17:54:56 +0700

    Tinh so can nang cua co the theo bmi done

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,26 @@
+package rikkei.academy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.println("Your weight (In kilogram): ");
+        weight = scanner.nextDouble();
+
+        System.out.println("Your height (In meter): ");
+        height = scanner.nextDouble();
+        bmi = weight / Math.pow(height, 2);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+        if (bmi < 18) {
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        } else if (bmi < 25.0) {
+            System.out.printf("%-20.2f%s", bmi, "Normal");
+        } else if (bmi < 30.0) {
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        } else {
+            System.out.printf("%-20.2f%s", bmi, "Obese");
+        }
+    }
+}
